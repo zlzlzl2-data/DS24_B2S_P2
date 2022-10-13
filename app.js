5ae34064de846ddbc6b418b67dcdc9dd9402c581
Liu Zhang <liu.zhang@theinformationlab.co.uk>
2022-10-13 16:36:47 +0100

    Final version

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Dummy webpage Console message
 console.log("Hello Back to School");
 
 // this let viz part is important
@@ -22,7 +23,23 @@ function initViz() {
 // Add an condition to trig viz loading
 document.addEventListener("DOMContentLoaded", initViz);
 
-// Define constant to use for button
+// Define constant to use for show & hide viz button
+const showVizButton = document.getElementById("showViz");
+const hideVizButton = document.getElementById("hideViz");
+
+// Define the function to show/hide viz
+function showViz() {
+  viz.show();
+}
+function hideViz() {
+  viz.hide();
+}
+
+// Add funntional response to show/hide buttons.
+showVizButton.addEventListener("click", showViz);
+hideVizButton.addEventListener("click", hideViz);
+
+// Define constant to use for export buttons
 const exportPDFbutton = document.getElementById("exportPDF");
 const exportPPTbutton = document.getElementById("exportPPT");
 
@@ -34,15 +51,17 @@ function exportPPTfunction() {
   viz.showExportPowerPointDialog();
 }
 
-// Add function to export buttons
+// Add functional response to export buttons
 exportPDFbutton.addEventListener("click", exportPDFfunction);
 exportPPTbutton.addEventListener("click", exportPPTfunction);
 
 // Add function to get user input values
+// Second level of text body below buttons
 function getRangeValues() {
   // Add function to get user input values
   const minValue = document.getElementById("minValue").value;
   const maxValue = document.getElementById("maxValue").value;
+  // Dummy to check the validate of above getElement
   console.log(minValue, maxValue);
 
   // Identify the worksheet
@@ -50,11 +69,18 @@ function getRangeValues() {
   // need to get active sheet, but this could be a dashboard or a worksheet
   const activeSheet = workbook.getActiveSheet();
   const sheets = activeSheet.getWorksheets();
+  // Dummy to check the list of sheets
   console.log(sheets);
 
   // Isolate sheets to filter and apply filter
-  const sheetToFilter = sheets[0];
-  sheetToFilter
+  // Map
+  const sheetToFilter0 = sheets[0];
+  sheetToFilter0
+    .applyRangeFilterAsync("SUM(Sales)", { min: minValue, max: maxValue })
+    .then(alert("viz filtered"));
+  // Bar
+  const sheetToFilter1 = sheets[1];
+  sheetToFilter1
     .applyRangeFilterAsync("SUM(Sales)", { min: minValue, max: maxValue })
     .then(alert("viz filtered"));
 }
